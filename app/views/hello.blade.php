bef3d59fbf0876dfb16f4e8195fe6b0307889b73
norwingc <norwingcruz@gmail.com>
2014-07-24 21:39:26 -0600

    segundo dia del taller
    controller, model, view

---
+++ app/views/hello.blade.php
@@ -0,0 +1,11 @@
+@extends('tamplates.maintemplate')
+
+@section('contenido')
+
+<div class="jumbotron">
+	<h1>Primer Crud Con Larevel</h1>	
+</div>
+<div class="container text-center">
+
+</div>
+@stop
